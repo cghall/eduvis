11a1bd4db4d8a2b423095f8bfbf7b243c3fbb2c1
cghall <christopherhall17@gmail.com>
2015-06-14 18:58:21 +0100

    use full school data csv

--- script.js
+++ script.js
@@ -1,22 +1,7 @@
-var csvString = "SCHNAME,PTAC5EM_PTQ_parsed\n\
-The Elmgreen School,0.31\n\
-Saint Gabriel's College,0.49\n\
-Evelyn Grace Academy,0.52\n\
-Lilian Baylis Technology School,0.52\n\
-Norwood School,0.57\n\
-Platanos College,0.57\n\
-Lambeth Academy,0.58\n\
-Dunraven School,0.6\n\
-London Nautical School,0.63\n\
-Archbishop Tenison's School,0.64\n\
-St Martin in the Fields High School for Girls,0.68\n\
-Bishop Thomas Grant Catholic Secondary School,0.72\n\
-La Retraite Roman Catholic Girls' School,0.8"
-
 var drawBar = function(records) {
     console.log(records);
     filteredData = _.filter(records.data, function(school) {
-        return school['PTAC5EM_PTQ_parsed'] < 0.6;
+        return school['LEA'] == 208;
     });
     
     var data = {
@@ -32,4 +17,5 @@ var drawBar = function(records) {
     var myBarChart = new Chart(ctx).Bar(data, {});
 };
 
-Papa.parse('http://cghall.github.io/eduvis/Lambeth_GCSE_score.csv', {download: true, header: true, complete: drawBar});
+Papa.parse('http://cghall.github.io/eduvis/School_data.csv',
+           {download: true, header: true, complete: drawBar, skipEmptyLines: true});
