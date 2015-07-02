feea077e78b0ca60c5127ac5ee753d7b20893d2f
Andrew Walker <awalkerdev@gmail.com>
2015-07-03 00:26:10 +0100

    remove js files in app folder from gulp include

--- gulpfile.js
+++ gulpfile.js
@@ -16,8 +16,6 @@ var requireJsRuntimeConfig = vm.runInNewContext(fs.readFileSync('src/app/require
         },
         include: [
             'requireLib',
-            'app/cookie-manager',
-            'app/data-load',
             'components/nav-bar/nav-bar',
             'components/home-page/home',
             'components/loading/loading',
