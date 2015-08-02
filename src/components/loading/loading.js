5f313c59220f721212e85f73a7706009151537aa
Andrew Walker <awalkerdev@gmail.com>
2015-08-02 18:55:51 +0100

    Update loading page with logo, new text and centering

--- src/components/loading/loading.js
+++ src/components/loading/loading.js
@@ -1,9 +1,39 @@
-define(['knockout', 'text!./loading.html'], function(ko, templateMarkup) {
+define(['knockout', 'underscore', 'text!./loading.html'], function (ko, _, templateMarkup) {
 
-  function Loading(params) {
+    function Loading(params) {
 
-  }
-  
-  return { viewModel: Loading, template: templateMarkup };
+    }
+
+    var ellipsis = {
+        'value': ['&nbsp;', '.', '..', '...'],
+        'count': 0,
+        'run': false,
+        'timer': null,
+        'element': '.ellipsis',
+        'start': function () {
+            var t = this;
+            this.run = true;
+            this.timer = setInterval(function () {
+                if (t.run) {
+                    $(t.element).html(t.value[t.count % t.value.length]).text();
+                    t.count++;
+                }
+            }, 500);
+        },
+        'stop': function () {
+            this.run = false;
+            clearInterval(this.timer);
+            this.count = 0;
+        }
+    };
+
+    var ellipsis_reverse = _.clone(ellipsis);
+    ellipsis_reverse.value = _.clone(ellipsis.value).reverse();
+    ellipsis_reverse.element = '.ellipsis-reverse';
+
+    ellipsis.start();
+    ellipsis_reverse.start();
+
+    return {viewModel: Loading, template: templateMarkup};
 
 });
