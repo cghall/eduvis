32786cbe811a7c534feaaa73b8441acb49696dcc
cghall <christopherhall17@gmail.com>
2015-08-31 15:34:21 +0100

    refactored code and added LA data
    - Updated mangle.py
    - Updated create_final.py
    - Added RECTYPE with SCH/LA options to final_data

--- mangling/create_final.py
+++ mangling/create_final.py
@@ -1,61 +1,28 @@
 import os
-import pandas as pd
-import itertools
 import re
+import pandas as pd
 
 META_PATH = os.path.join('..', 'data-src', 'meta_file.csv')
 SCHOOL_DATA_PATH = os.path.join('..', 'data-out', 'school_data.csv')
-
 OUTPUT_PATH = os.path.join('..', 'data-out', 'final_data.csv')
 
+# Create list of columns from meta_file and create final_data df
 meta_data = pd.read_csv(META_PATH)
-school_data = pd.read_csv(SCHOOL_DATA_PATH)
+final_data = (pd.read_csv(SCHOOL_DATA_PATH, usecols=meta_data['column'])
+              .convert_objects(convert_numeric=True))
 
-# Create list of columns from meta_file
-cols = meta_data['column'].tolist()
-cols = set(cols)
-cols = list(cols)
-
-# Create final_data df
-final_data = school_data[cols]
-
-# Create dict of cols with data_type
-col_type = dict(zip(meta_data.column,meta_data.data_type))
-
-# Create list of int columns
-int_cols = list(a for (a, b) in col_type.iteritems() if b == 'int')
-
-# Apply convert numeric to int columns
-def convert_numeric(cols):
-    for col in cols:
-        final_data[col] = final_data[col].convert_objects(convert_numeric=True)
-convert_numeric(int_cols)
-
-# Create dict of columns that can be aggregated with weighted_calc
-col_category = dict(zip(meta_data.column,meta_data.category))
-agg_cols_name = list(a for (a, b) in col_category.iteritems() if b == 'metric' or b == 'filter')
-
-col_weighted = dict(zip(meta_data.column,meta_data.weighted_value))
-agg_cols = dict((a, b) for (a, b) in col_weighted.iteritems() if a in agg_cols_name)
-
-# Perform weighted calculation
-for col in agg_cols_name:
-    weighted_calc = [b for (a, b) in agg_cols.iteritems() if a == col]
-    weighted_calc = ''.join(weighted_calc)
-    weighted_calc = re.sub(r"([A-Za-z]\w+)", r"final_data['\1']", weighted_calc)
-    final_data[col+'_weighted'] = eval(weighted_calc)
+# Do weighted calculations
+aggs = meta_data[meta_data.category.isin(('metric', 'filter'))]
+for column, formula in zip(aggs['column'], aggs['weighted_value']):
+    formula = re.sub(r"([A-Za-z]\w+)", r"final_data['\1']", formula)
+    final_data[column+'_weighted'] = eval(formula)
 
 # Add establishment group column to final_data
-establishment_mapping = {'Academy Converter': 'Academies', 'Community School' : 'LA maintained schools',
-                         'Academy Sponsor Led' : 'Academies', 'Foundation School' : 'LA maintained schools',
-                         'Voluntary Aided School' : 'LA maintained schools', 'Free Schools' : 'Free Schools',
-                         'Voluntary Controlled School' : 'LA maintained schools', 'Studio Schools' : 'Free Schools',
-                         'University Technical College' : 'Free Schools', 'City Technology College' : 'Academies'}
-
+establishment_mapping = {'Academy Converter': 'Academies', 'Community School': 'LA maintained schools',
+                         'Academy Sponsor Led': 'Academies', 'Foundation School': 'LA maintained schools',
+                         'Voluntary Aided School': 'LA maintained schools', 'Free Schools': 'Free Schools',
+                         'Voluntary Controlled School': 'LA maintained schools', 'Studio Schools': 'Free Schools',
+                         'University Technical College': 'Free Schools', 'City Technology College': 'Academies'}
 final_data['EstablishmentGroup'] = final_data['TypeOfEstablishment (name)'].replace(establishment_mapping)
 
-# Removed independent schools from final_data
-#final_data = final_data[final_data['EstablishmentGroup'] != 'Other Independent School']
-#inal_data = final_data[final_data['EstablishmentGroup'] != 'Other Independent Special School']
-
-final_data.to_csv(OUTPUT_PATH)
+final_data.to_csv(OUTPUT_PATH, index=False)
--- mangling/mangle.py
+++ mangling/mangle.py
@@ -1,83 +1,51 @@
 import os
-import pandas as pd
 import re
+import numpy as np
+import pandas as pd
 
-ENGLAND_KS4_PATH = os.path.join('..', 'data-src', 'dfe', 'england_ks4.csv')
-ENGLAND_CENSUS_PATH = os.path.join('..', 'data-src', 'dfe', 'england_census.csv')
+KS4_PATH = os.path.join('..', 'data-src', 'dfe', 'england_ks4.csv')
+CENSUS_PATH = os.path.join('..', 'data-src', 'dfe', 'england_census.csv')
 EDUBASE_PATH = os.path.join('..', 'data-src', 'dfe', 'edubase_data.csv')
 LEA_CODE_PATH = os.path.join('..', 'data-src', 'lea_names.csv')
-
+LAT_LONG_PATH = os.path.join('..', 'data-src', 'postcode_to_longlat.csv')
 OUTPUT_PATH = os.path.join('..', 'data-out', 'school_data.csv')
 
+# Read in CSVs
+ks4 = pd.read_csv(KS4_PATH)
+census = pd.read_csv(CENSUS_PATH)
+edubase = pd.read_csv(EDUBASE_PATH, dtype={'URN': np.int32})
+lea_codes = pd.read_csv(LEA_CODE_PATH)
+lat_longs = pd.read_csv(LAT_LONG_PATH)
 
-# Read in ks4 csv
-ks4_england = pd.read_csv(ENGLAND_KS4_PATH)
-# Filter out non-school records
-ks4_england = ks4_england[ks4_england['RECTYPE']==1]
-ks4_england = ks4_england[ks4_england['NFTYPE'] != 'IND']
-ks4_england = ks4_england[ks4_england['NFTYPE'] != 'INDSPEC']
+# Add record type flag and filter out independent and other schools
+record_type_map = {1: 'SCH',
+                   4: 'LA'}
+ks4['RECTYPE'] = ks4['RECTYPE'].map(record_type_map)
+is_school_la = ks4['RECTYPE'].isin({'SCH', 'LA'})
+is_independent = ks4['NFTYPE'].isin({'IND', 'INDSPEC'})
+ks4 = ks4[is_school_la & ~is_independent]
 
-# Read in census
-england_census = pd.read_csv(ENGLAND_CENSUS_PATH)
-# Read in edubase
-edubase = pd.read_csv(EDUBASE_PATH)
-# Convert URN from object to float
-england_census.URN = england_census.URN.convert_objects(convert_numeric=True)
-
-### Check for duplicate columns and remove from dfs before merge
-# Function for returning common elements
-def common(list1, list2):
-    return [el for el in list1 if el in list2]
-# Function for returning all occurrences
-def remove_all(list_name, val):
-    return [value for value in list_name if value != val]
-# Create column lists
-ks4_cols = list(ks4_england.columns)
-census_cols = list(england_census.columns)
-edubase_cols = list(edubase.columns)
-
-census_todrop = common(ks4_cols, census_cols)
-census_todrop.remove('URN')
-
-edubase_todrop = common(ks4_cols, edubase_cols) + common(census_cols, edubase_cols)
-edubase_todrop = remove_all(edubase_todrop, 'URN')
-
-england_census.drop(census_todrop, axis=1, inplace=True)
-edubase.drop(edubase_todrop, axis=1, inplace=True)
+# Check for duplicate columns and remove from dfs before merge
+keep_census_cols = census.columns.difference(ks4.columns[ks4.columns != 'URN'])
+keep_edubase_cols = edubase.columns.difference(ks4.columns[ks4.columns != 'URN'])
 
 # Merge data frames
-merged_df = pd.merge(ks4_england, england_census, on='URN', how='left')
-merged_df = pd.merge(merged_df, edubase, on='URN', how='left')
+merged_df = (ks4.merge(census[keep_census_cols], on='URN', how='left')
+                .merge(edubase[keep_edubase_cols], on='URN', how='left')
+                .merge(lea_codes, left_on='LEA', right_on='LEA_CODE', how='left')
+                .merge(lat_longs, left_on='PCODE', right_on='Address', how='left')
+                .rename(columns={'LEA_REGION': 'REGION'}))
 
-# Add Region to data frame
-lea_codes = pd.read_csv(LEA_CODE_PATH)
-
-def add_region(row, df):
-    lea_region_map = zip(df['LEA_CODE'], df['LEA_REGION'])
-    region = list(b for (a,b) in lea_region_map if a == row['LEA'])
-    region = ','.join(region)
-    return region
-                         
-merged_df['REGION'] = merged_df.apply (lambda row: add_region(row, lea_codes), axis = 1)
-                         
-# Replace LEA codes with descriptive code
-##lea_codes['NAME'] = lea_codes['LEA_NAME'] +' '+'('+lea_codes['LEA_CODE'].astype(str)+')'
-lea_dict = lea_codes.set_index('LEA_CODE')['LEA_NAME'].to_dict()
-merged_df['LEA_NAME'] = merged_df['LEA']
-merged_df = merged_df.replace({'LEA_NAME': lea_dict})
-
-# Function that parses percentage columns into float or None
+# Change percent column values to float
 def parse_percentage(value):
     percentages = re.findall(r'(\.?\d+\.?\d*)', str(value))
     if percentages:
         return float(percentages[0])
-    else:
-        return None
-# Apply parse function to merged data frame
-all_column_names = merged_df.columns.values
-percent_columns = [name for name in all_column_names
-                   if name.startswith('P') and name != 'PCODE' and '_parsed' not in name]
+    return None
+percent_columns = [col for col in merged_df.columns.values
+                   if col.startswith('P') and col != 'PCODE' and '_parsed' not in col]
 for column in percent_columns:
     merged_df[column] = merged_df[column].map(parse_percentage)
-# Create CSV from df
-merged_df.to_csv(OUTPUT_PATH)
+
+# Create output CSV
+merged_df.to_csv(OUTPUT_PATH, index=False)
